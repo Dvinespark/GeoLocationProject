a94ec72c7d9ed4258f8f33f0caed646d76740668
Shubhas <nimtsubash@gmail.com>
2022-02-01 10:57:41 -0500

    data fetched completed. writing in xl in progress

--- job_category_pull.py
+++ job_category_pull.py
@@ -1,7 +1,27 @@
 # selenium imports
+from asyncio import wait_for
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+# headless configuration
+options = Options()
+options.headless = True
+
+
+def get_job_list(link):
+    print("Navigating to :" + str(link))
+    jobs = []
+    s = Service('./chromedriver.exe')
+    driver = webdriver.Chrome(service=s, options=options)
+    driver.maximize_window()
+    driver.get(link)
+    jobs_elements = driver.find_elements(By.XPATH, ".//a[@class='pb0']")
+    for job in jobs_elements:
+        jobs.append(job.text)
+    driver.close()
+    return jobs
 
 
 def pull_data(site_url):
@@ -10,6 +30,7 @@ def pull_data(site_url):
     jobs = []
     s = Service('./chromedriver.exe')
     driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
     driver.get(site_url)
 
     # getting divs with category
@@ -21,17 +42,22 @@ def pull_data(site_url):
         category.append(category_text[0].text)
         category_li_elements = element.find_elements(By.TAG_NAME, 'li')
         temp_sub_category = []
+        temp_jobs = []
         for each_li in category_li_elements:
             temp_sub_category.append(each_li.text)
+            print("Navigating to .... " + each_li.text)
+            temp_jobs = get_job_list(each_li.find_elements(By.TAG_NAME, 'a')[0].get_attribute('href'))
+            # driver.execute_script("window.history.go(-1)")
         sub_category.append(temp_sub_category)
+        jobs.append(temp_jobs)
 
     print(category)
     print(sub_category)
-
-
-    # loop through each li elements to get sub categories
+    print(jobs)
 
     driver.close()
+
+    # do some logic here to make a flat lists and then write it in xl file
 
 
 if __name__ == "__main__":
